2743d40161327e25128ad7e9cc12c18852ca0977
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-08-28 21:35:08 -0700

    xorsubarray

--- array/subarrayxork.py
+++ array/subarrayxork.py
@@ -3,8 +3,23 @@
 # Given an array of integers nums and an integer k, return the total number of subarrays whose XOR equals to k.
 # Examples:
 # Input : nums = [4, 2, 2, 6, 4], k = 6
-# Output : 4
+# Output : 4 
 # Explanation : The subarrays having XOR of their elements as 6 are [4, 2],  [4, 2, 2, 6, 4], [2, 2, 6], and [6]
 # Input :nums = [5, 6, 7, 8, 9], k = 5
 # Output : 2
 # Explanation : The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]
+
+def xor(arr,target):
+    count=0
+    n=len(arr)
+    for i in range(n):
+        xor=0
+        for j in range(n):
+            xor=xor ^ arr[j]
+            if xor==target:
+                count+=1
+    return count
+arr=[4,2,2,6,4]
+target=6
+result=xor(arr,target)
+print(result)
