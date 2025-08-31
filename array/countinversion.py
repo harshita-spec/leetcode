87dee2de981b12825b213e8422b8d4d6595cc4a2
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-08-31 11:56:46 -0700

    count inversion

--- array/countinversion.py
+++ array/countinversion.py
@@ -18,4 +18,41 @@
 # Input: nums = [-10, -5, 6, 11, 15, 17]
 # Output: 0
 # Explanation: nums is sorted, hence no inversions present.
-
+import math
+def merge(arr,low,mid,high):
+    temp=[]
+    left=low
+    right=mid+1
+    count=0
+    while left<=mid and right<=high:
+        if arr[left]<=arr[right]:
+            temp.append(arr[left])
+            left+=1
+        else:
+            temp.append(arr[right])
+            count+=(mid-left+1)
+            right+=1
+    while left<=mid:
+        temp.append(arr[left])
+        left+=1
+    while right<=high:
+        temp.append(arr[right])
+        right+=1
+    for i in range(low,high):
+        arr[i]=temp[i-low]
+    return count
+def mergesort(arr,low,high):
+    count=0
+    if low>=high:
+        return count
+    mid=math.floor((low+high)/2)
+    count+=mergesort(arr,low,mid)
+    count+=mergesort(arr,mid+1,high)
+    count+=merge(arr,low,mid,high)
+    return count
+def inversion(arr,n):
+    return mergesort(arr,0,n-1)
+arr=[2, 3, 7, 1, 3, 5]
+n=len(arr)
+result=inversion(arr,n)
+print(result)
