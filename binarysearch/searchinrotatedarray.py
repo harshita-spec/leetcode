9dcfc3819f768da0fad097b2458b58285f614f68
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-09-02 19:45:39 -0700

    search in rotated array

---
+++ binarysearch/searchinrotatedarray.py
@@ -0,0 +1,43 @@
+# 33. Search in Rotated Sorted Array
+
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+
+def rotate(arr,n,target):
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            return mid
+        if arr[low]<=arr[mid]:
+            if arr[low]<=target and arr[mid]>=target:
+                high=mid-1
+            else:
+                low=mid+1
+        else:
+            if arr[mid]<=target and arr[high]>=target:
+                low=mid+1
+            else:
+                high=mid-1
+    return -1
+arr=[7,8,9,1,2,3,4,5,6]
+n=len(arr)
+target=2
+result=rotate(arr,n,target)
+print(result)
