f224176e8071eebb617ee9986242d563e0d7d315
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-09-09 22:33:17 -0700

    sort or not

---
+++ array/arraysort.py
@@ -0,0 +1,22 @@
+# Check if the Array is Sorted II
+
+# Given an array nums of n integers, return true if the array nums is sorted in non-decreasing order or else false.
+# Examples:
+# Input : nums = [1, 2, 3, 4, 5]
+# Output : true
+# Explanation : For all i (1 <= i <= 4) it holds nums[i] <= nums[i+1], hence it is sorted and we return true.
+# Input : nums = [1, 2, 1, 4, 5]
+# Output : false
+# Explanation : For i == 2 it does not hold nums[i] <= nums[i+1], hence it is not sorted and we return false.
+
+def check(nums,n):
+        for i in range(1,len(nums)):
+            if nums[i]>=nums[i-1]:
+                return True
+            else:
+                return False
+        return True
+nums=[1, 2, 3, 4, 5]
+n=len(nums)
+result=check(nums,n)
+print(result)
