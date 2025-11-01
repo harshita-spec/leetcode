20e2d76f4dd6895bff7d30b4bef2018bcf623527
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-11-01 11:01:32 -0700

    valid anagram

---
+++ string/validanagram.py
@@ -0,0 +1,20 @@
+# Valid Anagram
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+# Examples:
+# Input : s = "anagram" , t = "nagaram"
+# Output : true
+# Explanation :
+# We can rearrange the characters of string s to get string t as frequency of all characters from both strings is same.
+# Input : s = "dog" , t = "cat"
+# Output : false
+# Explanation :
+# We cannot rearrange the characters of string s to get string t as frequency of all characters from both strings is not same.
+from collections import Counter
+def anagram(s,t):
+    return Counter(s)==Counter(t)
+s = "anagram" 
+t = "nagaram"
+print(anagram(s,t))
