34e7e35aaaa182a00516338f564a608895537d16
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-11-10 22:36:23 -0800

    subarray with 0

--- array/subarraywithsum0.py
+++ array/subarraywithsum0.py
@@ -26,7 +26,7 @@ def maxLen(A,n):
             else:
                 mpp[sum] = i
     return maxi
-a=[1,-1,3,2,-2,-8,1,7,10,23]
+a=[1,-1,3,2,-2,-8,1,7,10,12,23]
 n=len(a)
 result=maxLen(a,n)
 print(result)
