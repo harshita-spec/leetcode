967764bd50c3d39b303ea8d61570eb9f4f5b5a73
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-11-10 22:35:36 -0800

    subarray sum

--- array/subarraysumequalsk.py
+++ array/subarraysumequalsk.py
@@ -25,7 +25,7 @@ def findAllSubarraysWithGivenSum(arr, k):
         cnt += mpp[remove]
         mpp[preSum] += 1
     return cnt
-arr = [3, 1, 2, 4]
+arr = [3, 1, 2, 4,7]
 k = 6
 cnt = findAllSubarraysWithGivenSum(arr, k)
 print("The number of subarrays is:", cnt)
