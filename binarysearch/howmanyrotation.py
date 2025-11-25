7bd26e040a886efecc4bc220bd142e635d9601ab
Harshita Laxman Moger <mogerharshi@gmail.com>
2025-11-24 21:26:09 -0800

    array rotation

--- binarysearch/howmanyrotation.py
+++ binarysearch/howmanyrotation.py
@@ -32,7 +32,7 @@ def minimum(arr,n):
                 ans=arr[mid]
             high=mid-1
     return index
-arr=[4, 5, 6, 7, 0, 1, 2, 3]
+arr=[4, 5, 6, 7,8,9,0, 1, 2, 3]
 n=len(arr)
 result=minimum(arr,n)
 print(result)
