a07ac2da55cbad1c6077c4344863311ca5559c1b
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 10:43:35 -0800

    single ele

---
+++ array/singlenumber.py
@@ -0,0 +1,23 @@
+# Single Number - I
+# Given an array of nums of n integers. Every integer in the array appears twice except one integer. Find the number that appeared once in the array.
+# Example 1
+# Input : nums = [1, 2, 2, 4, 3, 1, 4]
+# Output : 3
+# Explanation : The integer 3 has appeared only once.
+# Example 2
+# Input : nums = [5]
+# Output : 5
+# Explanation : The integer 5 has appeared only once
+
+#brute
+def single(arr):
+    for i in range(len(arr)):
+        count=0
+        num=arr[i]
+        for j in range(len(arr)):
+            if arr[j]==num:
+                count+=1
+        if count==1:
+            return num
+arr=[1,2,2,4,3,1,4]
+print(single(arr))
