8740290cb114aec0df1323a60264a5c98d98972b
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 10:46:11 -0800

    single ele

--- array/singlenumber.py
+++ array/singlenumber.py
@@ -10,14 +10,33 @@
 # Explanation : The integer 5 has appeared only once
 
 #brute
+# def single(arr):
+#     for i in range(len(arr)):
+#         count=0
+#         num=arr[i]
+#         for j in range(len(arr)):
+#             if arr[j]==num:
+#                 count+=1
+#         if count==1:
+#             return num
+        
+#better
 def single(arr):
+    mpp={}
     for i in range(len(arr)):
-        count=0
-        num=arr[i]
-        for j in range(len(arr)):
-            if arr[j]==num:
-                count+=1
-        if count==1:
-            return num
-arr=[1,2,2,4,3,1,4]
+        if arr[i] in mpp:
+            mpp[arr[i]]+=1
+        else:
+            mpp[arr[i]]=1
+    for i ,value in mpp.items():
+        if value==1:
+            return i
+
+#optimal
+def single(arr):
+    res=0
+    for i in range(len(arr)):
+        res=res^arr[i]
+    return res
+arr=[1,2,2,4,3,3,5,1,4]
 print(single(arr))
