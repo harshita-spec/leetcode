88234aed8bd940e495c774252235bd26014800ac
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 09:59:16 -0800

    move zeros

--- array/movezeros.py
+++ array/movezeros.py
@@ -14,15 +14,27 @@
 # All 3 zeroes are moved to the end and the order of the other elements stay the same
 
 #brute force
+# def move0(arr):
+#     temp=[]
+#     for i in range(len(arr)):
+#         if arr[i]!=0:
+#             temp.append(arr[i])
+#     for i in range(len(temp)):
+#         arr[i]=temp[i]
+#     for i in range(len(temp),len(arr)):
+#         arr[i]=0
+#     return arr
+
+#optimal
 def move0(arr):
-    temp=[]
     for i in range(len(arr)):
+        if arr[i]==0:
+            j=i
+            break
+    for i in range(j+1,len(arr)):
         if arr[i]!=0:
-            temp.append(arr[i])
-    for i in range(len(temp)):
-        arr[i]=temp[i]
-    for i in range(len(temp),len(arr)):
-        arr[i]=0
+            arr[i],arr[j]=arr[j],arr[i]
+            j+=1
     return arr
 arr=[0, 0, 0, 1, 3, -2]
 print(move0(arr))
