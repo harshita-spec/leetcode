60b9290af60687b6a39079db4efa5eae36ae1096
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 11:09:42 -0800

    longest subarray sum

--- array/subarraysumequalsk.py
+++ array/subarraysumequalsk.py
@@ -12,20 +12,33 @@
 # Input: nums = [1,2,3], k = 3
 # Output: 2
 
-from collections import defaultdict
-def findAllSubarraysWithGivenSum(arr, k):
-    n = len(arr) 
-    mpp = defaultdict(int)
-    preSum = 0
-    cnt = 0
-    mpp[0] = 1 
-    for i in range(n):
-        preSum += arr[i]
-        remove = preSum - k
-        cnt += mpp[remove]
-        mpp[preSum] += 1
-    return cnt
+#brute force
+def findsubarray(arr,k):
+    lenh=0
+    for i in range(len(arr)):
+        sum=0
+        for j in range(i,len(arr)):
+            sum+=arr[j]
+            if sum==k:
+                lenh=max(lenh,j-i+1)
+    return lenh
+
+#optimal
+# from collections import defaultdict
+# def findAllSubarraysWithGivenSum(arr, k):
+#     n = len(arr) 
+#     mpp = defaultdict(int)
+#     preSum = 0
+#     cnt = 0
+#     mpp[0] = 1 
+#     for i in range(n):
+#         preSum += arr[i]
+#         remove = preSum - k
+#         cnt += mpp[remove]
+#         mpp[preSum] += 1
+#     return cnt
 arr = [3, 1, 2, 4,7]
 k = 6
-cnt = findAllSubarraysWithGivenSum(arr, k)
-print("The number of subarrays is:", cnt)
+print(findsubarray(arr,k))
+# cnt = findAllSubarraysWithGivenSum(arr, k)
+# print("The number of subarrays is:", cnt)
