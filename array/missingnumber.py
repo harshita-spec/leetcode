0507cc235c3f9eefd421f22b4110798577a2ae70
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 10:08:32 -0800

    missing number

--- array/missingnumber.py
+++ array/missingnumber.py
@@ -12,12 +12,16 @@
 # nums contains 0, 1, 2, 4, 5, 6 thus leaving 3 as the only missing number in the range [0, 6]
 
 def miss(arr,n):
-    sum=(n*(n+1))/2
-    s2=0
+    # sum=(n*(n+1))/2
+    # s2=0
+    # for i in range(n):
+    #     s2+=arr[i]
+    # return int(s2-sum)
     for i in range(n):
-        s2+=arr[i]
-    return int(s2-sum)
-arr=[1,3,4,5,6]
+        if i not in arr:
+            return i
+    return n  
+arr=[0, 1, 2, 3,4, 5, 6]
 n=len(arr)
 result=miss(arr,n)
 print(result)
