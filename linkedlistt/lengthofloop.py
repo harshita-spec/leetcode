a0201ae88b92fd0e387fb754163b43b6e99ce015
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-01-26 22:28:57 -0800

    length of loop

---
+++ linkedlistt/lengthofloop.py
@@ -0,0 +1,47 @@
+# Length of loop in LL
+# Given the head of a singly linked list, find the length of the loop in the linked list if it exists. Return the length of the loop if it exists; otherwise, return 0.
+
+# A loop exists in a linked list if some node in the list can be reached again by continuously following the next pointer. Internally, pos is used to denote the index (0-based) of the node from where the loop starts.
+
+# Note that pos is not passed as a parameter.
+# Example 1
+#ut: head -> 1 -> 2 -> 3 -> 4 -> 5, pos = 1
+# Output: 4
+# Explanation: 2 -> 3 -> 4 -> 5 - >2, length of loop = 4.
+# Example 2
+# Input: head -> 1 -> 3 -> 7 -> 4, pos = -1
+# Output: 0
+# Explanation: No loop is present in the linked list.
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class SLL:
+    def __init__(self):
+        self.head=None
+    
+    #brute force
+    def length(self):
+        mpp={}
+        temp=self.head
+        timer=0
+        while temp is not None:
+            if temp in mpp:
+                return timer-mpp[temp]
+            mpp[temp]=timer
+            timer+=1
+            temp=temp.next
+        return 0
+n1=Node(5)
+sll=SLL()
+sll.head=n1
+n2=Node(10)
+n1.next=n2
+n3=Node(15)
+n2.next=n3
+n4=Node(25)
+n3.next=n4
+n4.next=n2
+print(sll.length())
