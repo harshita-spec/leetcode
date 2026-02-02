92c3ad878ddbfca79c1ccdaebccd30fed079c6f7
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-02 19:43:45 +0530

    longest substring without repeating char

--- slidingwinandtwopointer/longestsubstringwithoutrepcharacter.py
+++ slidingwinandtwopointer/longestsubstringwithoutrepcharacter.py
@@ -10,17 +10,37 @@
 # Output : 2
 # Explanation : The answers are "ab" , "bc". Both have maximum length 2.
 
-def substring(s):
+#brute force
+# def substring(s):
+#     maxlen=0
+#     for i in range(len(s)):
+#         hash=[0]*256
+#         for j in range(i,len(s)):
+#             if hash[ord(s[j])]==1:
+#                 break
+#             leng=j-i+1
+#             maxlen=max(leng,maxlen)
+#             hash[ord(s[j])]=1
+#     return maxlen
+# s="abcddabac"
+# result=substring(s)
+# print(result)
+
+#optimal
+def longestsubstring(s):
+    n=len(s)
+    hash=[0]*256
+    left=0
+    right=0
     maxlen=0
-    for i in range(len(s)):
-        hash=[0]*256
-        for j in range(i,len(s)):
-            if hash[ord(s[j])]==1:
-                break
-            leng=j-i+1
-            maxlen=max(leng,maxlen)
-            hash[ord(s[j])]=1
+    while right<n:
+        if hash[ord(s[right])]!=-1:
+            if hash[ord(s[right])]>=left:
+                left=hash[ord(s[right])]+1
+        maxlen=max(maxlen,right-left+1)
+        hash[ord(s[right])]=right
+        right+=1
     return maxlen
-s="abcddabac"
-result=substring(s)
+s="aaabbbccc"
+result=longestsubstring(s)
 print(result)
