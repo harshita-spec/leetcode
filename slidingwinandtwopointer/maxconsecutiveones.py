c4b6f605f18171f9a05f8cdb517e2c4e12501106
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-03 23:06:40 +0530

    max consecutive ones

--- slidingwinandtwopointer/maxconsecutiveones.py
+++ slidingwinandtwopointer/maxconsecutiveones.py
@@ -44,7 +44,25 @@ def maxOnes(nums,k):
         if zero<=k:
             maxlen=max(maxlen,r-l+1)
     return maxlen
+
+#optimal 
+def maxOness(nums,k):
+    l=0
+    zero=0
+    maxlen=0
+    for r in range(len(nums)):
+        if nums[r]==0:
+            zero+=1
+        while zero>k:
+            if nums[l]==0:
+                zero-=1
+            l+=1
+        if zero<=k:
+            maxlen=max(maxlen,r-l+1)
+    return maxlen
+
 nums=[1,1,1,0,0,0,1,1,1,1,0]
 k=3
 print(maxones(nums,k))  
 print(maxOnes(nums,k))
+print(maxOness(nums,k))
