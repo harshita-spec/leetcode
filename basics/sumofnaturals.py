9d3cca664ad0b3c116b7b7f076f32908c7ed9f23
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-10 22:06:28 +0530

    sum of n natural num

---
+++ basics/sumofnaturals.py
@@ -0,0 +1,21 @@
+# Program for sum of n natural numbers
+
+# Given a positive integer n, find the sum of the first n natural numbers.
+
+# Examples : 
+
+# Input: n = 3
+# Output: 6
+# Explanation: 1 + 2 + 3 = 6
+
+# Input: n = 5
+# Output: 15 
+# Explanation:  1 + 2 + 3 + 4 + 5 = 15
+
+def sumofnaturals(n):
+    sum=0
+    for i in range(n+1):
+        sum+=i
+    return sum
+n=6
+print(sumofnaturals(n))
