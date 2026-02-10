6b7540105551fa3403840add9d144a87281549d9
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-10 22:15:07 +0530

    swap

---
+++ basics/swap2numbers.py
@@ -0,0 +1,23 @@
+# Swap Two Numbers
+
+# Given two numbers a and b, the task is to swap them.
+
+# Examples:
+
+# Input: a = 2, b = 3
+# Output: a = 3, b = 2
+
+# Input: a = 20, b = 0
+# Output: a = 0, b = 20
+
+# Input: a = 10, b = 10
+# Output: a = 10, b = 10
+
+def swap(a,b):
+    temp=a
+    a=b
+    b=temp
+    return a,b
+a=10
+b=20
+print(swap(a,b)) 
