3ed3aeb64ced1469692846edcf567ffc85105676
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-23 22:47:30 +0530

    longest repeating char

--- slidingwinandtwopointer/longestrepeatingchar.py
+++ slidingwinandtwopointer/longestrepeatingchar.py
@@ -14,3 +14,20 @@
 # The new string is "AABBBBA". The substring "BBBB" is the answer.
 # There are other ways to achieve this answer.
 
+def characterReplacement( s, k):
+        freq = {}
+        left = 0
+        max_freq = 0
+        max_len = 0
+        for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
+            max_freq = max(max_freq, freq[s[right]])
+            while (right - left + 1) - max_freq > k:
+                freq[s[left]] -= 1
+                left += 1
+            max_len = max(max_len, right - left + 1)
+        return max_len
+s="AABABBA"
+k=3
+result=characterReplacement(s,k)
+print(result)
