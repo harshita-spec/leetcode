be77e9c0a3345f20a32af339032c415a82de0fe6
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-02-25 22:18:28 +0530

    two sum

--- array/twosum.py
+++ array/twosum.py
@@ -6,22 +6,16 @@
 # You can return the answer in any order.
 
 # Example 1:
-
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 # Example 2:
-
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
 # Example 3:
-
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
- 
-
 # Constraints:
-
 # 2 <= nums.length <= 104
 # -109 <= nums[i] <= 109
 # -109 <= target <= 109
@@ -29,15 +23,13 @@
  
 def Twosum(nums,target):
     for i in range(len(nums)):
-        for j in range(i,len(nums)):
+        for j in range(i+1,len(nums)):
             if nums[i]+nums[j]==target:
                 return [i,j]
-    return []
+    return [-1,-1]
 nums=[3,2,4]
 target=6
 print(Twosum(nums,target))
-
-    
   
 def twoSum(nums, target):
         hash_map = {}
@@ -47,7 +39,6 @@ def twoSum(nums, target):
                 return [hash_map[complement], index]
             hash_map[num] = index
         return []
-
 
 def twosum(arr,target):
     n=len(arr)
