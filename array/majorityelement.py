b11273d1351888946b413b8d15990ef6b470e5f1
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-01 22:43:33 +0530

    majority element

--- array/majorityelement.py
+++ array/majorityelement.py
@@ -11,6 +11,19 @@
 # Output: 1
 # Explanation: The number 1 appears 4 times in the 6 sized array
 
+def majority(arr):
+    for i in range(len(arr)):
+        count=0
+        for j in range(len(arr)):
+            if arr[i]==arr[j]:
+                count+=1
+        if count>len(arr)/2:
+            return arr[i]
+    return -1
+arr=[7, 0, 0, 1, 7, 2, 7, 7,7 ,7,7,7]
+result=majority(arr)
+print(result) 
+
 def Majority(arr):
     hash_map={}
     for i in range(len(arr)):
