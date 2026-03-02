8155298c86bd7588f8dee48cd307d307dc88bfc6
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-02 22:02:35 +0530

    kadane's algorithm

--- array/kadanesalgorithm.py
+++ array/kadanesalgorithm.py
@@ -10,6 +10,21 @@
 # Input: nums = [-2, -3, -7, -2, -10, -4]
 # Output: -2
 # Explanation: The element on index 0 or index 3 make up the largest sum when taken as a subarray
+
+def maximum(arr):
+    n=len(arr)
+    maxi=-float('inf')
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            sum=0
+            for k in range(i,j+1):
+                sum+=arr[k]
+            maxi=max(sum,maxi)
+    return maxi 
+arr=[2, 3, 5, -2, 7, -4]
+result=maximum(arr)
+print(result)
+
 import sys
 def kadane(arr):
     n=len(arr)
