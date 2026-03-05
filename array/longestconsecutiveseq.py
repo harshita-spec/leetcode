cd163662384bb22fba9e473bb53a1be222f99cd7
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-05 19:16:03 +0530

    longest consecutive sequence

--- array/longestconsecutiveseq.py
+++ array/longestconsecutiveseq.py
@@ -13,11 +13,29 @@
 # Input: nums = [1,0,1,2]
 # Output: 3
 
+def elements(a):
+    n=len(a)
+    longest=1
+    for i in range(n):
+        x=a[i]
+        cnt=1
+        while ls(a,x+1):
+            x+=1
+            cnt+=1
+        longest=max(longest,cnt)
+    return longest
+def ls(a,x):
+    for i in range(len(a)):
+        if a[i]==x:
+            return True
+    return False
+a=[0,3,7,2,5,8,4,6,0,1]
+print(elements(a))
+
 def longestSuccessiveElements(a):
     n = len(a)
     if n == 0:
         return 0
-
     longest = 1
     st = set()
     for i in range(n):
@@ -33,5 +51,5 @@ def longestSuccessiveElements(a):
     return longest
 
 a = [100, 200, 1, 2, 3, 4]
-ans = longestSuccessiveElements(a)
-print("The longest consecutive sequence is", ans)
+print(longestSuccessiveElements(a))
+
