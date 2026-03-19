ec5975bd87b528c298c2b53cc9b3907c76d1d4a3
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-19 13:46:26 +0530

    merge 2 sorted array

--- array/merge2sirtarray.py
+++ array/merge2sirtarray.py
@@ -14,6 +14,7 @@
 # Output: [-7, -3, -1, 0, 2, 7, 8]
 # Explanation: The merged array is: [-7, -3, -1, 0, 2, 7, 8], where [0, 2, 7, 8] are from nums1 and [-7, -3, -1] are from nums2
 
+#brute force approach
 def merge(arr1,arr2,n,m):
     arr3=[0]*(n+m)
     i=0
@@ -27,6 +28,14 @@ def merge(arr1,arr2,n,m):
             arr3[index]=arr2[j]
             j+=1
         index+=1
+    while i<n:
+        arr3[index]=arr1[i]
+        i+=1
+        index+=1
+    while j<m:      
+        arr3[index]=arr2[j]
+        j+=1
+        index+=1
     return arr3
 nums1 = [-5, -2, 4, 5]
 nums2 = [-3, 1, 8]
@@ -36,6 +45,42 @@ result=merge(nums1,nums2,n,m)
 print(result)
 print()
 
+def merge(arr1,arr2,n,m):
+    arr3=[0]*(n+m)
+    i=0
+    j=0
+    index=0
+    while i<n and j<m:
+        if arr1[i]<arr2[j]:
+            arr3[index]=arr1[i]
+            i+=1
+        else:
+            arr3[index]=arr2[j]
+            j+=1
+        index+=1
+    while i<n:
+        arr3[index]=arr1[i]
+        i+=1
+        index+=1
+    while j<m:
+        arr3[index]=arr2[j]
+        j+=1
+        index+=1
+    for i in range(len(arr3)):
+        if i<n:
+            arr1[i]=arr3[i]
+        else:
+            arr2[i-n]=arr3[i]   
+arr1=[-5, -2, 4, 5]
+arr2=[-3, 1, 8]
+n=len(arr1)
+m=len(arr2)
+merge(arr1,arr2,n,m)
+print(arr1)
+print(arr2) 
+print()
+
+#optimal approach
 def merged(arr1,arr2,n,m):
     left=n-1
     right=0
