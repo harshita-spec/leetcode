b5d3e634267543b058d54a3231d3825c45719909
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-20 16:18:26 +0530

    ratate array

--- array/rotateimage.py
+++ array/rotateimage.py
@@ -9,6 +9,19 @@
 # Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 # Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 
+#brute force
+def matrotate(matrix):
+    n=len(matrix)
+    ans = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            ans[j][n-1-i] = matrix[i][j]
+    return ans
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ans = matrotate(matrix)
+print(ans)      
+
+#optimal 
 def rotate(matrix):
     n = len(matrix)
     for i in range(n):
