7b56d47ca12dc16b4091dc3c65a769b1baa818ff
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-21 19:25:09 +0530

    subarray sum of k

--- array/subarraysumequalsk.py
+++ array/subarraysumequalsk.py
@@ -12,6 +12,21 @@
 # Input: nums = [1,2,3], k = 3
 # Output: 2
 
+#brute
+def sumofk(arr,k):
+    count=0
+    for i in range(len(arr)):
+        for j in range(i,len(arr)):
+            sum=0
+            for k in range(i,j+1):
+                sum+=arr[k]
+            if sum==k:
+                count+=1
+    return count
+arr = [3, 1, 2, 4,7]
+k = 6
+print(sumofk(arr,k))
+
 #optimal
 from collections import defaultdict
 def findAllSubarraysWithGivenSum(arr, k):
