d5fe69fb180ae30d85601d1b5ca0891827dacea7
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-25 22:49:02 +0530

    stacks

--- stacksandqueues/impofstacks.py
+++ stacksandqueues/impofstacks.py
@@ -3,7 +3,8 @@ class stacks:
         self.st=[0]*size
         self.top=-1
         self.size=size
-
+    
+    #append a element at the top of the stack and increase the size of stack by 1
     def push(self,data):
         if self.top==self.size-1:
             print("stack overflow")
@@ -11,6 +12,7 @@ class stacks:
             self.top+=1
             self.st[self.top]=data
     
+    #remove the top element of the stack and decrease the size of stack by 1
     def pop(self):
         if self.top==-1:
             print("stack underflow")
@@ -18,16 +20,19 @@ class stacks:
             data=self.st[self.top]
             self.top-=1
             return data
-        
+
+    #return the top element of the stack without removing it 
     def peek(self):
         if self.top==-1:
             print("stack underflow")
         else:
             return self.st[self.top]
         
+    #return the current size of the stack
     def sizeofst(self):
         return self.top+1
-        
+
+    #print the elements of the stack from top to bottom    
     def printst(self):
         for i in range(self.top+1):
             print(self.st[i],end=" ")
