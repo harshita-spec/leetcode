9a7eb94b3d8eb3d5f9d51516a618933eea651817
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-03-29 12:35:28 +0530

    valid parenthesis

---
+++ stacksandqueues/validparenthesis.py
@@ -0,0 +1,18 @@
+def isValid(s):
+    stack = []  
+    for ch in s:
+        if ch in "({[":
+            stack.append(ch) 
+        else:
+            if not stack:
+                return False 
+            top = stack.pop()
+            if (ch == ')' and top == '(') or \
+                (ch == ']' and top == '[') or \
+                (ch == '}' and top == '{'):
+                continue
+            else:
+                return False
+    return not stack 
+s="()[]{}"
+print(isValid(s))
