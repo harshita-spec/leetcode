c621bf845f02d00c8b6f7d767e0f63006736697b
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-04-01 21:43:42 +0530

    contains duplicates

---
+++ array/containduplicate.py
@@ -0,0 +1,26 @@
+# 217. Contains Duplicate
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+# Explanation:
+# The element 1 occurs at the indices 0 and 3.
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
+# Explanation:
+# All elements are distinct.
+# Example 3:
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+def dup(arr):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i]==arr[j]:
+                return True
+    return False
+nums = [1,1,1,3,3,4,3,2,4,2]
+print(dup(nums))
