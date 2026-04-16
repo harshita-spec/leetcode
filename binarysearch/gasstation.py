8077e7175fc7a05298562856e91d7c667835d8b2
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-04-16 23:56:42 +0530

    gas station

--- binarysearch/gasstation.py
+++ binarysearch/gasstation.py
@@ -39,7 +39,7 @@ def countofstation(arr, mid):
     return count
 
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 n = len(arr)
 k = 4
 result = gasstation(arr, n, k)
