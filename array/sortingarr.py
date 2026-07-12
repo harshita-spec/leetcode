ba1c341466405e7c2aeb1ef1e24f76254b1f10f0
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-12 18:21:18 +0530

    sorting by using for loop

---
+++ array/sortingarr.py
@@ -0,0 +1,9 @@
+# sorting a array in ascending order using for loop
+arr=[64, 34, 25, 12, 22, 11, 90]
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] > arr[j]:
+            temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+print("Sorted array is:", arr)
