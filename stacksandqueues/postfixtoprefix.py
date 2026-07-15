eb05d4eec0f50cb93d653c7b14df595b82b8f88b
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-15 11:52:12 +0530

    postfix to prefix

---
+++ stacksandqueues/postfixtoprefix.py
@@ -0,0 +1,16 @@
+# postfix to infix conversion using stack
+def postfixToInfix(s):
+    st=[]
+    for c in s:
+        if c.isalnum():
+            st.append(c)
+        else:
+            t1=st[-1]
+            st.pop()
+            t2=st[-1]
+            st.pop()
+            s= c + t2 + t1
+            st.append(s)
+    return st[-1]
+s="ab+cde+**"
+print(postfixToInfix(s))
