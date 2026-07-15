a2d80cbf0c79fab140afca2d6dbc8d31ed64fb0c
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-15 12:37:49 +0530

    next greater element

--- stacksandqueues/nextgraeterele.py
+++ stacksandqueues/nextgraeterele.py
@@ -15,6 +15,7 @@
 # Output: [8, -1, 1, 3, -1]
 # Explanation: In the array, the next larger element to 6 is 8, for 8 there is no larger elements hence it is -1, for 0 it is 1 , for 1 it is 3 and then for 3 there is no larger element on the right and hence -1.
 
+#brute force approach
 def nextGreaterElement(arr):
     n = len(arr)
     nge = [0]*n
@@ -28,3 +29,19 @@ def nextGreaterElement(arr):
     return nge
 arr = [6, 8, 0, 1, 3]
 print(nextGreaterElement(arr))
+
+# optimal approach using stack
+def nextGreaterelement(arr):
+    nge=[0]*len(arr)
+    st=[]
+    for i in range(len(arr)-1,-1,-1):
+        while st and st[-1]<=arr[i]:
+            st.pop()
+        if not st:
+            nge[i]=-1
+        else:
+            nge[i]=st[-1]
+        st.append(arr[i])
+    return nge
+arr = [1, 3, 2, 4]
+print(nextGreaterelement(arr))
