b3aa4223299a0d3503e9da950a83f8d107d369cf
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-16 09:48:14 +0530

    remove element

--- array/removelement.py
+++ array/removelement.py
@@ -37,7 +37,17 @@ def removeElement(nums, val):
                 nums[k]=nums[i]
                 k+=1
         return nums[0:k]
-
 nums = [0,1,2,2,3,0,4,2]
 val = 2
 print(removeElement(nums, val))
+
+def removeelement(nums, val):
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+print(removeelement(nums, val))
