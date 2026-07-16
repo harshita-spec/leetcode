cdd643485cc7547779b7dafbeeb79ef4c6285f9c
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-16 16:25:56 +0530

    3sum closest

---
+++ array/3sumclosest.py
@@ -0,0 +1,36 @@
+# 16. 3Sum Closest
+
+# Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+# Return the sum of the three integers.
+# You may assume that each input would have exactly one solution.
+# Example 1:
+# Input: nums = [-1,2,1,-4], target = 1
+# Output: 2
+# Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+# Example 2:
+# Input: nums = [0,0,0], target = 1
+# Output: 0
+# Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+def threeSumClosest(nums, target):
+        closesum = nums[0]+nums[1]+nums[2]
+        nums=sorted(nums)
+        summ=0
+        for i in range(len(nums)-2):
+            j=i+1
+            k=len(nums)-1
+            while j<k:
+                summ = nums[i]+nums[j]+nums[k]
+                if abs(summ - target) < abs(closesum - target):
+                    closesum = summ
+                if summ==target:
+                    return summ
+                elif summ>target:
+                    k-=1
+                else:
+                    j+=1
+        return closesum
+nums=[-1,2,1,-4]
+target=1
+print(threeSumClosest(nums, target))
