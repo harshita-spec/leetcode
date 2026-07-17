810eb42bae7c5799584661ceedbd06e62b7e84ea
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-17 17:39:39 +0530

    rotate 90 deg

--- array/rotate90deg.py
+++ array/rotate90deg.py
@@ -26,8 +26,8 @@ print(matrotate(matrix))
 #optimal 
 def mat(matrix):
     n=len(matrix)
-    for i in range(n):
-        for j in range(i):
+    for i in range(n-1):
+        for j in range(i+1,n):
             matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
     for i in range(n):
         rev(matrix[i])
