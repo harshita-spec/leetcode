0c47c1bf6c0b45b0ac5d3105a37c55b0d7b14814
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-21 15:51:51 +0530

    merge 2 sorted array without space

--- array/merge2sirtarray.py
+++ array/merge2sirtarray.py
@@ -45,42 +45,8 @@ result=merge(nums1,nums2,n,m)
 print(result)
 print()
 
-def merge(arr1,arr2,n,m):
-    arr3=[0]*(n+m)
-    i=0
-    j=0
-    index=0
-    while i<n and j<m:
-        if arr1[i]<arr2[j]:
-            arr3[index]=arr1[i]
-            i+=1
-        else:
-            arr3[index]=arr2[j]
-            j+=1
-        index+=1
-    while i<n:
-        arr3[index]=arr1[i]
-        i+=1
-        index+=1
-    while j<m:
-        arr3[index]=arr2[j]
-        j+=1
-        index+=1
-    for i in range(len(arr3)):
-        if i<n:
-            arr1[i]=arr3[i]
-        else:
-            arr2[i-n]=arr3[i]   
-arr1=[-5, -2, 4, 5]
-arr2=[-3, 1, 8]
-n=len(arr1)
-m=len(arr2)
-merge(arr1,arr2,n,m)
-print(arr1)
-print(arr2) 
-print()
 
-#optimal approach
+#optimal approach 1
 def merged(arr1,arr2,n,m):
     left=n-1
     right=0
@@ -93,6 +59,7 @@ def merged(arr1,arr2,n,m):
             break
     arr1.sort()
     arr2.sort()
+
 arr1=[1,3,5,7]
 arr2=[0,2,6,8,9]
 n=len(arr1)
@@ -107,6 +74,7 @@ for i in range(m):
     print(arr2[i], end=" ")
 print() 
 
+# optimal 2
 def overlap(arr1,arr2,n,m):
     len=n+m
     gap=(len//2)+(len%2)
@@ -128,6 +96,7 @@ def overlap(arr1,arr2,n,m):
 def swap(arr1,arr2,ind1,ind2):
     if arr1[ind1]>arr2[ind2]:
         arr1[ind1],arr2[ind2]=arr2[ind2],arr1[ind1]
+
 arr1=[1,3,5,7]
 arr2=[0,2,6,8,9]
 n=len(arr1)
