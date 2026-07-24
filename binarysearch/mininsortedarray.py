c754335f90cad968fdf81665c2c0c30905b85344
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-24 18:54:43 +0530

    minimum in rotated sorted array

--- binarysearch/mininsortedarray.py
+++ binarysearch/mininsortedarray.py
@@ -39,3 +39,21 @@ def minimum(arr):
 arr=[4, 5, 6, 7, 0, 1, 2]
 result=minimum(arr)
 print(result)
+
+def mini(arr):
+    n=len(arr)
+    low=0
+    high=n-1
+    ans=float('inf')
+    while low<=high:
+        mid=(low+high)//2
+        if arr[low]<=arr[mid]:
+            ans=min(ans,arr[low])
+            low=mid+1
+        else:
+            ans=min(ans,arr[mid])
+            high=mid-1
+    return ans
+arr=[4, 5, 6, 7, 1, 2]
+result=mini(arr)
+print(result)
