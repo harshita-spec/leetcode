e6bc461e090fdd805bc9a6153c8a67a8217399f9
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-07-24 18:50:04 +0530

    minimum in sorted array

--- binarysearch/mininsortedarray.py
+++ binarysearch/mininsortedarray.py
@@ -36,6 +36,6 @@ def minimum(arr):
             ans=min(ans,arr[mid])
             high=mid-1
     return ans
-arr=[4, 5, 6, 7, 0, 1, 2, 3]
+arr=[4, 5, 6, 7, 0, 1, 2]
 result=minimum(arr)
 print(result)
