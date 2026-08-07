4721b439f558ee30da8e699b23613613d8a021df
Harshita Laxman Moger <mogerharshi@gmail.com>
2026-08-07 10:26:29 +0530

    search in 2d mat

--- binarysearch/searchin2dmat.py
+++ binarysearch/searchin2dmat.py
@@ -17,13 +17,13 @@ def matrixs(matrix,target):
     col= m-1
     while row < n and col >=0:
         if matrix[row][col]==target:
-            return True
+            return [row,col]
         elif matrix[row][col]<target:
             row+=1
         else:
             col-=1
-    return False
+    return [-1,-1]
 matrix= [ [1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30] ]
-target = 20
+target = 11
 result=matrixs(matrix,target)
 print(result)
